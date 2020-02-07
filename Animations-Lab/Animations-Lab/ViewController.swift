deb68c305a19471385e3f92c907a4f6b612c0572
Ahad Islam <ahad.islam624@gmail.com>
2020-02-07 18:39:30 -0500

    implemented ability to move left and right, also another stepper to manipulate time. created picker view

--- Animations-Lab/Animations-Lab/ViewController.swift
+++ Animations-Lab/Animations-Lab/ViewController.swift
@@ -45,7 +45,7 @@ class ViewController: UIViewController {
     
     lazy var rightButton: UIButton = {
         let b = UIButton()
-        b.setTitle("Move square left", for: .normal)
+        b.setTitle("Move square right", for: .normal)
         b.setTitleColor(.black, for: .normal)
         b.backgroundColor = .cyan
         b.addTarget(self, action: #selector(animateSquareRight(sender:)), for: .touchUpInside)
@@ -72,6 +72,11 @@ class ViewController: UIViewController {
         return s
     }()
     
+    lazy var pickerView: UIPickerView = {
+        let pv = UIPickerView()
+        return pv
+    }()
+    
     lazy var blueSquareHeightConstaint: NSLayoutConstraint = {
         blueSquare.heightAnchor.constraint(equalToConstant: 200)
     }()
@@ -87,6 +92,8 @@ class ViewController: UIViewController {
     lazy var blueSquareCenterYConstraint: NSLayoutConstraint = {
         blueSquare.centerYAnchor.constraint(equalTo: view.centerYAnchor)
     }()
+    
+    let animations = [UIView.AnimationOptions.curveEaseIn, .curveEaseInOut, .curveLinear, .transitionCurlUp, .transitionFlipFromLeft]
     
     var time: Double = 2
     var distance: Double = 150
@@ -104,38 +111,44 @@ class ViewController: UIViewController {
     
     @IBAction func changeDistance(sender: UIStepper) {
         distance = sender.value
+        print(pickerView.selectedRow(inComponent: 0))
     }
     
     @IBAction func animateSquareUp(sender: UIButton) {
         let oldOffset = blueSquareCenterYConstraint.constant
         blueSquareCenterYConstraint.constant = oldOffset - CGFloat(distance)
-        UIView.animate(withDuration: time) { [unowned self] in
+        let option = animations[pickerView.selectedRow(inComponent: 0)]
+        UIView.animate(withDuration: time, delay: 0, options: [option], animations: { [unowned self] in
             self.view.layoutIfNeeded()
-        }
+        }, completion: nil)
     }
     
     @IBAction func animateSquareDown(sender: UIButton) {
         let oldOffet = blueSquareCenterYConstraint.constant
         blueSquareCenterYConstraint.constant = oldOffet + CGFloat(distance)
-        UIView.animate(withDuration: time) { [unowned self] in
+        let option = animations[pickerView.selectedRow(inComponent: 0)]
+        UIView.animate(withDuration: time, delay: 0, options: [option], animations: { [unowned self] in
             self.view.layoutIfNeeded()
-        }
+        }, completion: nil)
     }
     
     @IBAction func animateSquareLeft(sender: UIButton) {
         let oldOffset = blueSquareCenterXConstraint.constant
         blueSquareCenterXConstraint.constant = oldOffset - CGFloat(distance)
-        UIView.animate(withDuration: time) { [unowned self] in
+        let option = animations[pickerView.selectedRow(inComponent: 0)]
+        UIView.animate(withDuration: time, delay: 0, options: [option], animations: { [unowned self] in
             self.view.layoutIfNeeded()
-        }
+        }, completion: nil)
     }
     
     @IBAction func animateSquareRight(sender: UIButton) {
         let oldOffset = blueSquareCenterXConstraint.constant
         blueSquareCenterXConstraint.constant = oldOffset + CGFloat(distance)
-        UIView.animate(withDuration: time) { [unowned self] in
+        let option = animations[pickerView.selectedRow(inComponent: 0)]
+        UIView.animate(withDuration: time, delay: 0, options: [option], animations: { [unowned self] in
             self.view.layoutIfNeeded()
-        }
+        }, completion: nil)
+        
     }
     
     private func addSubviews() {
@@ -145,6 +158,7 @@ class ViewController: UIViewController {
     
     private func configureConstraints() {
         constrainBlueSquare()
+        setupPickerView()
         constrainButtonStackView()
         constrainButtonStackView2()
     }
@@ -169,7 +183,7 @@ class ViewController: UIViewController {
         view.addSubview(stackview)
         stackview.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            stackview.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            stackview.bottomAnchor.constraint(equalTo: pickerView.topAnchor),
             stackview.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             stackview.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             stackview.heightAnchor.constraint(equalToConstant: 50.0)])
@@ -188,8 +202,52 @@ class ViewController: UIViewController {
                 stackview.leadingAnchor.constraint(equalTo: view.leadingAnchor),
                 stackview.trailingAnchor.constraint(equalTo: view.trailingAnchor),
                 stackview.heightAnchor.constraint(equalToConstant: 50.0)])
-        }
+    }
+    
+    private func setupPickerView() {
+        pickerView.dataSource = self
+        pickerView.delegate = self
+        
+        view.addSubview(pickerView)
+        pickerView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            pickerView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            pickerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            pickerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            pickerView.heightAnchor.constraint(equalToConstant: 100)])
+    }
     
 }
 
+extension ViewController: UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return animations.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        switch row {
+        case 0:
+            return "Curve Ease In"
+        case 1:
+            return "Curve Ease In Out"
+        case 2:
+            return "Curve Linear"
+        case 3:
+            return "Transition Curl Up"
+        case 4:
+            return "Transition Flip From Left"
+        default :
+            return nil
+        }
+    }
+    
+}
+
+extension ViewController: UIPickerViewDelegate {
+    
+}
 
